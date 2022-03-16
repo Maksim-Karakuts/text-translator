2f38f002ccbda42d35233d1a2117bddeb294a8aa
Maksim Karakuts <maksim.karakuts@gmail.com>
2022-03-16 12:55:07 +0300

    Initial commit

---
+++ src/test/java/com/tinkoff/maksim/karakuts/text/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinkoff.maksim.karakuts.text.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
